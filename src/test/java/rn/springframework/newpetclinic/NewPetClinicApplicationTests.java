37576090b7942dd4eaca5ae5a8c771f124a27d8d
roland <azathoth667@yahoo.de>
2020-09-20 11:02:56 +0200

    Initial Project Add

---
+++ src/test/java/rn/springframework/newpetclinic/NewPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rn.springframework.newpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
